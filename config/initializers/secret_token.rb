4380160978744e482e9e5a206838eb620f3fdcd6
rafaeldl <rafaeldll@gmail.com>
2012-09-17 00:15:20 -0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapaentrega::Application.config.secret_token = 'c34cf4c7c7f8cc3b1175b4f678cf70c4442c0d95d67dabc21041d6ec50389ae5e17a0cdb1f449a51efc071919582212b1f5a208a5f9980874220d3703d4dace9'
